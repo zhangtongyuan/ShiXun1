247f864201286fb7b0cf6d4b830daea497b78279
zhangtongyuan <1140038681@qq.com>
2016-12-07 21:51:53 +0800

    短信验证

---
+++ umm/app/src/main/java/com/bwie/um/duanxin/DuanXinUtils.java
@@ -0,0 +1,29 @@
+package com.bwie.um.duanxin;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+
+/**
+ * 作用
+ * 作者
+ * 日期： 2016/12/7
+ */
+public class DuanXinUtils {
+    private static SharedPreferences sp;
+
+    //存
+    public static void saveTime(Context context, String name, Long l) {
+        if (sp == null) {
+            context.getSharedPreferences("appinfo.xml", Context.MODE_PRIVATE);
+        }
+        sp.edit().putLong(name, l).commit();
+    }
+
+    //取
+    public static long getsaveTime(Context context, String name) {
+        if (sp == null) {
+            sp = context.getSharedPreferences("appinfo.xml", Context.MODE_PRIVATE);
+        }
+        return sp.getLong(name, 0);
+    }
+}
---
+++ umm/app/src/main/java/com/bwie/um/wxapi/WXEntryActivity.java
@@ -0,0 +1,12 @@
+package com.bwie.um.wxapi;
+
+import com.umeng.weixin.callback.WXCallbackActivity;
+
+/**
+ * 作用
+ * 作者
+ * 日期： 2016/12/7
+ */
+public class WXEntryActivity extends WXCallbackActivity {
+
+}
